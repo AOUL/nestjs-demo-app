39d8b47fca0ed1bcb63d427153bd2494705a4f56
HICHAM ELAOULA <aoul.hicham@gmail.com>
2023-02-11 00:40:35 +0100

    perform validation pipes

--- src/products/create-product.dto.ts
+++ src/products/create-product.dto.ts
@@ -1,5 +1,11 @@
+import { IsString, Length, IsInt, IsNotEmpty } from 'class-validator';
+
 export class CreateProductDTO {
+  @IsString()
+  @Length(5, 50)
   name: string;
+  @IsInt({ message: 'The quantity must be an integer' })
   qte: number;
+  @IsNotEmpty()
   price: number;
 }
